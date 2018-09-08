16d52069ade0beb4280eba11f0f54a3abd5cdfe5
Adam U Flores <watsaname1999@gmail.com>
2018-09-07 14:58:56 -1000

    Now accept 0s after , and doens't accept at beginning.

--- Assn2.java
+++ Assn2.java
@@ -1,13 +1,19 @@
 public class Assn2{
+    String fLoc;
     public static void main (String[] args){
-        //File dir  =  new File("a2_input");
-        String regex = "\\$(\\*)*([1-9]\\d?\\d?,?)*.\\d\\d"; //"$(\\*)*(0|([1-9]\\d*),*)*.\\d{2}";
+        // if(args.isEmpty()){
+        //     System.out.print("Enter file location: ");
+        //     System.in(fLoc);
+        // else
+        //     fLoc = args[0];
+        //File dir  =  new File(args[0]);
+        String regex = "\\$(\\*)*[1-9](\\d?\\d?,?)*.\\d\\d"; //"$(\\*)*(0|([1-9]\\d*),*)*.\\d{2}";
         //Can't acccept 0 hundreds place
-        String testString = "$**2,345.67";  
+        String testString = "$**22,45.67";  
         if(testString.matches(regex))
             System.out.println("Match!");
         else
-            System.out.println("Fail!");
+            System.out.println(testString + ": Fail!");
         
         //For (File f : )
     }
