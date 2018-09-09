f5a3ccb85c961cd227397920cac62cf0f19bb1df
Adam U Flores <watsaname1999@gmail.com>
2018-09-09 08:32:07 -1000

    Current Work

--- Assn2.java
+++ Assn2.java
@@ -1,20 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Assn2{
-    String fLoc;
     public static void main (String[] args){
-        // if(args.isEmpty()){
-        //     System.out.print("Enter file location: ");
-        //     System.in(fLoc);
-        // else
-        //     fLoc = args[0];
-        //File dir  =  new File(args[0]);
-        String regex = "\\$(\\*)*[1-9](\\d?\\d?,?)*.\\d\\d"; //"$(\\*)*(0|([1-9]\\d*),*)*.\\d{2}";
-        //Can't acccept 0 hundreds place
-        String testString = "$**22,45.67";  
-        if(testString.matches(regex))
-            System.out.println("Match!");
+        String fLoc;
+        BufferedReader file;
+        Scanner cin = new Scanner(System.in);
+        if(args.length == 0){
+            System.out.print("Enter file location: ");
+            fLoc = cin;
+        }
         else
-            System.out.println(testString + ": Fail!");
+            fLoc = args[0];
+        file = new BufferedReader(fLoc);
+        File dir  =  new File(fLoc);
+        String regex = "\\$(\\*)*[1-9](\\d?\\d?,?)*.\\d\\d"; //"$(\\*)*(0|([1-9]\\d*),*)*.\\d{2}";
+        // String testString = "$**022,045.67";  
+        for (File f : dir.listFiles() ){
+            if(testString.matches(regex))
+                System.out.println("Match!");
+            else
+                System.out.println(testString + ": Fail!");
+        }
         
-        //For (File f : )
-    }
+        }
 }
