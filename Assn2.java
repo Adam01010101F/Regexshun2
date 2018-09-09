dc2d478cb019672e5836a81487db14a0770aff74
Adam U Flores <watsaname1999@gmail.com>
2018-09-09 14:06:48 -0700

    Finished Java Regex
    Java program can now check for financial quantities.

--- Assn2.java
+++ Assn2.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 public class Assn2{
     public static void main (String[] args){
         String fLoc;
-        BufferedReader file;
+        File file;
+        BufferedReader reader;
         Scanner cin = new Scanner(System.in);
-        if(args.length == 0){
-            System.out.print("Enter file location: ");
-            fLoc = cin;
-        }
-        else
-            fLoc = args[0];
-        file = new BufferedReader(fLoc);
-        File dir  =  new File(fLoc);
-        String regex = "\\$(\\*)*[1-9](\\d?\\d?,?)*.\\d\\d"; //"$(\\*)*(0|([1-9]\\d*),*)*.\\d{2}";
-        // String testString = "$**022,045.67";  
-        for (File f : dir.listFiles() ){
-            if(testString.matches(regex))
-                System.out.println("Match!");
+            try{
+            if(args.length == 0){
+                System.out.print("Enter file location: ");
+                fLoc = cin.nextLine();
+            }
             else
-                System.out.println(testString + ": Fail!");
-        }
+                fLoc = args[0];
+            reader = new BufferedReader(new FileReader(fLoc));
+            String regex = "\\$(\\*)*(([1-9](\\d?\\d?,?)*)|0).\\d\\d"; //"$(\\*)*(0|([1-9]\\d*),*)*.\\d{2}";
+            // String testString = "$**022,045.67"; 
+            while (reader.ready()){
+                if(reader.readLine().matches(regex))
+                    System.out.println("Match!");
+                else
+                    System.out.println("Fail!");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }   
+
         
         }
 }
