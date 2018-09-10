ecf57f982674309a503b545c34640ad1b2b4b70d
Adam U Flores <watsaname1999@gmail.com>
2018-09-10 06:36:00 -1000

    C++ regex working

--- Assn2.cpp
+++ Assn2.cpp
@@ -5,7 +5,9 @@
 
 int main(){
     // std::ifstream input = new ifStream("a2_input.txt")
-    std::regex r = ("\\$(\\*)*[1-9](\\d?\\d?,?)*.\\d\\d");
-    std::string testString = "\\$(\\*)*(([1-9](\\d?\\d?,?)*)|0).\\d\\d";
-    // if(std::regex_match())
+    std::regex r ("\\$(\\*)*(([1-9](\\d?\\d?,?)*)|0).\\d\\d");
+    std::string test = "$**2,018.75";
+    if(std::regex_match(test, r)){
+        std::cout<<"Match!";
+    }
 }
