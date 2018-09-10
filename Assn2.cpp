50fde92521c282c6b30106a7b3a35988eb2baee4
Adam U Flores <watsaname1999@gmail.com>
2018-09-10 07:42:22 -1000

    CPP MVP Finished
    CPP can read file and judge regex

--- Assn2.cpp
+++ Assn2.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <string>
-#include <istream>
+#include <fstream>
 #include <regex>
 
 int main(){
-    // std::ifstream input = new ifStream("a2_input.txt")
+    std::ifstream input("a2_input.txt");
     std::regex r ("\\$(\\*)*(([1-9](\\d?\\d?,?)*)|0).\\d\\d");
-    std::string test = "$**2,018.75";
-    if(std::regex_match(test, r)){
-        std::cout<<"Match!";
+    std::string test = "$**2,345.67";
+    try{    
+        while(input>>test){    //input.is_open() && !input.eof()
+            // std::getline(input, test);
+            std::cout<<test<<std::endl;
+            if(std::regex_match(test, r)){
+                std::cout<<"Match!\n";
+            }else{
+                std::cout<<"Fail!\n";
+            }
+
+        }
+    }catch(int e){
+        std::cout<<"You caught bug, "<<e<<std::endl;
     }
+    input.close();
+    
 }
